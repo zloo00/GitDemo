f86c289087e89364e85b2c8c44bc83a6b885c305
Алуа Жолдыкан <aluazholdykan@MacBook-Air-Alua-3.local>
2025-02-23 14:42:38 +0500

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Алуа Жолдыкан on 23.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitDemoTests/GitDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoTests.swift
+//  GitDemoTests
+//
+//  Created by Алуа Жолдыкан on 23.02.2025.
+//
+
+import Testing
+@testable import GitDemo
+
+struct GitDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
